e7477e10df7de4aceca7f35b68c759f997ef7956
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-31 19:01:52 +0530

    Adding Coupon Number Program

---
+++ CouponNumber/CouponNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouponNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the logical programs");
+            Console.ReadLine();
+            CouponNumbers.Generate();
+        }
+    }
+}
