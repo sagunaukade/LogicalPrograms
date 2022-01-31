aacf71a90b8db00351a01e154f1a8f17ca07c0fd
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-31 19:04:09 +0530

    Adding Simulate Stopwatch Program

---
+++ SimulateStopwatchProgram/SimulateStopwatchProgram/SimulateStopwatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateStopwatchProgram
+{
+    internal class SimulateStopwatch
+    {
+        public static System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+        private static int ch;
+
+        public static void StartWatch()
+        {
+            do
+            {
+                Console.WriteLine("1. Start\n2. Stop \n3. Elapsed \n4. Exit");
+                Console.Write("Enter Your Choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        watch.Start();
+                        break;
+                    case 2:
+                        watch.Stop();
+                        break;
+                    case 3:
+                        double elapsedTime = Math.Round((double)watch.ElapsedMilliseconds / 1000, 2);
+                        Console.WriteLine("Elapsed Time is : " + elapsedTime + " Seconds");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (ch < 3);
+        }
+    }
+}
+    
